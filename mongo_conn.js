f6f102ef8d24656f8df955f56766c5759b37fde7
Joseph Newmann <newmann.joseph@gmail.com>
2020-07-27 09:28:59 -0400

    revert server to separate repo

---
+++ mongo_conn.js
@@ -0,0 +1,23 @@
+const { MongoClient } = require('mongodb');
+
+require('dotenv').config();
+const { DB_NAME, DB_KEY, DB_PORT } = process.env;
+
+//let url = `mongodb://${DB_NAME}:${DB_KEY}@${DB_NAME}.mongo.cosmos.azure.com:${DB_PORT}/?ssl=true&appName=@${DB_NAME}@`;
+let local_url = 'mongodb://127.0.0.1:27017/nuc-classrooms';
+let _db;
+
+module.exports = {
+    connectToServer: () => {
+        MongoClient.connect(local_url, (err, client) => {
+            if(err) { console.log(err); }
+            else { 
+                _db = client.db('nuc-classrooms');
+            }
+        })
+    },
+
+    getDb: () => {
+        return _db;
+    }
+}
